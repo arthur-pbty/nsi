16a195bfbe9ea7181d1c71008fbd21c47123ac99
Tutur33 <arthur.puechberty@outlook.com>
2023-11-28 22:49:04 +0100

    exercise 1.3 update

--- Activity_3_python/exercises_1/exercise_1_3.py
+++ Activity_3_python/exercises_1/exercise_1_3.py
@@ -1,5 +1,10 @@
-from math import *
+# Display the numerical value of √(4.63 - 15/16)
+# Compare with your calculator.
+
+from math import sqrt
+
 a = sqrt(4.6**3 - 15/16)
 print(a)
+
 if a == 9.818273779030608:
-    print("C'est bien égale")
+    print("It's equal to my calculator.")
