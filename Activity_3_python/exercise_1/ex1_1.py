ab8dff4fa67f7739c23bdb741800fefce3b82015
Tutur33 <arthur.puechberty@outlook.com>
2023-11-28 22:21:53 +0100

    Exercise 1.1 update

--- Activity_3_python/exercise_1/ex1_1.py
+++ Activity_3_python/exercise_1/ex1_1.py
@@ -1,2 +1,12 @@
-fichier = 536
-print("Fichier :", fichier, "kio", fichier * 2**10, "octets",fichier * 8 * 2**10, "bits")
+# Display the size in bytes and bits of a 536 kio file.
+# 1 kio (1 kibioctet) = 210 bytes
+# 1 byte = 1 byte = 8 bits
+
+bool = True
+while bool:
+   try:
+      fichier = int(input("What is the size of your file in kio ? "))
+      print("Fichier :", fichier, "kio", "=", fichier * 2**10, "octets", "=", fichier * 8 * 2**10, "bits")
+      bool = False
+   except:
+      print("You had to enter a number. Example: 536")
